407f681c07c7430efcfe4bc1cd0542c254f903ad
Daniel Gawne <danielgawne@gmail.com>
2017-10-15 15:38:35 -0400

    added solutions to 00, 01

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(farenheit)
+  celcius = (farenheit - 32) / 9 * 5
+  return celcius
+end
+
+def ctof(celcius)
+  farenheit = (celcius * 1.8) + 32
+  return farenheit
+end
