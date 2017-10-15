b6026a6439ca369d1605f2f787dbb19d57447d7a
Daniel Gawne <danielgawne@gmail.com>
2017-10-15 16:02:12 -0400

    Added 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(a, b)
+  sum = a + b
+  return sum
+end
+
+def subtract(a, b)
+  sub = a - b
+  return sub
+end
+
+def sum array
+  sum = 0
+  array.each {|num| sum += num }
+  return sum
+end
